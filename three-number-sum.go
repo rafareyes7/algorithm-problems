9053b9c1566b8bf4eae91c35aaadb1885af0f387
Rafael Reyes <rmreyesc@falabella.cl>
2020-03-29 19:45:36 -0300

    3Sum target : validates repeated elements.

--- three-number-sum.go
+++ three-number-sum.go
@@ -6,7 +6,7 @@ import (
 	"sort"
 )
 
-/* Write a function that takes in a non-empty array of distinct integers and an integer representing
+/* Write a function that takes in a non-empty array of integers and an integer representing
 a target sum.
 
 The function should find all triplets in the array that sum up to the target, and return a
@@ -19,14 +19,29 @@ If no three numbers sum up to the target sum, the function should return an empt
 example:
 
 	input: [12,3,1,2,-6,5,-8,6], 0
-	output: [[-8,2,6][-8,3,5][-6,1,5]] */
+	output: [[-8,2,6][-8,3,5][-6,1,5]]
+
+*/
 
 func threeNumberSum(array []int, target int) [][]int {
 	sort.Ints(array)
 	triplets := [][]int{}
 	for first := 0; first < len(array)-2; first++ {
+
+		if first > 0 && array[first-1] == array[first] {
+			// duplicated elem found -> [2, first:3, first+1:3, 5, ...]
+			continue
+		}
+
 		mid, left := first+1, len(array)-1
 		for mid < left {
+
+			if mid > first+1 && array[mid-1] == array[mid] {
+				// duplicated elem found -> [2, 3, 3, first:5, first+1:6, mid-1:7, mid:7, ...]
+				mid++
+				continue
+			}
+
 			currentSum := array[first] + array[mid] + array[left]
 			if currentSum < target {
 				mid++
@@ -62,5 +77,16 @@ func main() {
 		fmt.Printf("want: %v, got: %v", want, got)
 	}
 
-	fmt.Println("All test cases passed.")
+	want = [][]int{[]int{-2, 0, 2}}
+	got = threeNumberSum([]int{-2, 0, 0, 2, 2}, 0)
+	if !reflect.DeepEqual(want, got) {
+		fmt.Printf("want: %v, got: %v", want, got)
+	}
+
+	want = [][]int{[]int{0, 0, 0}}
+	got = threeNumberSum([]int{0, 0, 0}, 0)
+	if !reflect.DeepEqual(want, got) {
+		fmt.Printf("want: %v, got: %v", want, got)
+	}
+
 }
